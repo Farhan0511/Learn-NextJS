654c7104d64c9de527ff15ac38483f763c725c73
farhan0511 <programingsukses@gmail.com>
2026-06-19 17:56:05 +0700

    add jsonplaceholder api online

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,10 +1,30 @@
-function page() {
-  return (
-    <>
-        <h1>About Us</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic molestiae ipsa odio non! Fugit dolorum facere laudantium exercitationem vero, inventore laborum ipsam tempora numquam iste molestiae sequi blanditiis minima saepe vel quisquam rem error cumque.</p>
-    </>
-  )
+async function getUserAbout() {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=3",
+  );
+  const data = await res.json(); 
+  return data; 
 }
 
-export default page
+async function page() {
+  // ✅ async
+  const post = await getUserAbout(); 
+
+  return (
+    <>
+      <div>
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam labore porro voluptatem ut nobis ullam sequi itaque nam fugit placeat.</p>
+      </div>
+      <div className="post-list">
+        {post.map((post) => (
+          <div key={post.id} className="post-item">
+            <h1>{post.title}</h1>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default page;
